510510095811ef4c7e3505dadbd597033888fd59
Marcos Corsi <marcosvcorsi@gmail.com>
2021-04-20 21:05:58 -0300

    test: add create podcast usecase

---
+++ test/domain/useCases/createPodcast/CreatePodcastUseCase.spec.ts
@@ -0,0 +1,60 @@
+import { IFindPodcastByNameRepository } from '@/data/protocols/IFindPodcastByNameRepository';
+import { Podcast } from '@/domain/entities/Podcast';
+import { CreatePodcastUseCase } from '@/domain/useCases/createPodcast/CreatePodcastUseCase';
+import { CreatePodcastParams } from '@/domain/useCases/createPodcast/ICreatePodcastUseCase';
+
+const mockCreatePodcastParams = (): CreatePodcastParams => ({
+  name: 'anyname',
+  title: 'anytitle',
+  links: ['anylink'],
+});
+
+const mockPodcast = (): Podcast => ({
+  id: 'anyid',
+  name: 'anyname',
+  description: 'anydesc',
+  links: ['anylink'],
+});
+
+const mockFindPodcastByNameRepository = () => {
+  class FindPodcastByNameRepositoryStub
+    implements IFindPodcastByNameRepository {
+    async findByName(): Promise<Podcast> {
+      return mockPodcast();
+    }
+  }
+
+  return new FindPodcastByNameRepositoryStub();
+};
+
+describe('CreatePodcastUseCase Tests', () => {
+  let createPodcastUseCase: CreatePodcastUseCase;
+  let findPodcastByNameRepository: IFindPodcastByNameRepository;
+
+  beforeEach(() => {
+    findPodcastByNameRepository = mockFindPodcastByNameRepository();
+    createPodcastUseCase = new CreatePodcastUseCase(
+      findPodcastByNameRepository
+    );
+  });
+
+  it('should call FindPodcastByNameRepository with correct values', async () => {
+    const params = mockCreatePodcastParams();
+
+    const findSpy = jest.spyOn(findPodcastByNameRepository, 'findByName');
+
+    await createPodcastUseCase.create(params);
+
+    expect(findSpy).toHaveBeenCalledWith(params.name);
+  });
+
+  it('should throw if FindPodcastByNameRepository throws', async () => {
+    const params = mockCreatePodcastParams();
+
+    jest
+      .spyOn(findPodcastByNameRepository, 'findByName')
+      .mockRejectedValueOnce(new Error());
+
+    await expect(createPodcastUseCase.create(params)).rejects.toThrow();
+  });
+});
