1ab905930efb7761b8b0d24283403848dac4e0b0
Marcos Corsi <marcosvcorsi@gmail.com>
2021-04-22 21:15:36 -0300

    chore: add env config and test config

--- src/main/app/index.ts
+++ src/main/app/index.ts
@@ -1,8 +1,8 @@
 import cors from 'cors';
 import express from 'express';
 import 'express-async-errors';
-import 'dotenv/config';
 
+import '@/main/config/env';
 import '@/infra/database/mongodb';
 
 import { routes } from './routes';
