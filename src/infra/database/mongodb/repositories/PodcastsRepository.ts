ee2a3c9f6a149816ddd9f19c1358773a576878f8
Marcos Corsi <marcosvcorsi@gmail.com>
2021-04-27 21:48:21 -0300

    feat: add find podcasts repository

--- src/infra/database/mongodb/repositories/PodcastsRepository.ts
+++ src/infra/database/mongodb/repositories/PodcastsRepository.ts
@@ -1,13 +1,20 @@
 import { ICreatePodcastRepository } from '@/data/protocols/ICreatePodcastRepository';
 import { IFindPodcastByNameRepository } from '@/data/protocols/IFindPodcastByNameRepository';
+import {
+  FindPodcastsRepositoryParams,
+  IFindPodcastsRepository,
+} from '@/data/protocols/IFindPodcastsRepository';
 import { Podcast } from '@/domain/entities/Podcast';
 import { CreatePodcastParams } from '@/domain/useCases/createPodcast/ICreatePodcastUseCase';
 
-import { map } from '../helpers/mapper';
+import { map, mapCollection } from '../helpers/mapper';
 import { Podcast as PodcastModel } from '../schemas/PodcastSchema';
 
 export class PodcastsRepository
-  implements ICreatePodcastRepository, IFindPodcastByNameRepository {
+  implements
+    ICreatePodcastRepository,
+    IFindPodcastByNameRepository,
+    IFindPodcastsRepository {
   async create(data: CreatePodcastParams): Promise<Podcast> {
     const podcast = await PodcastModel.create(data);
 
@@ -18,5 +25,16 @@ export class PodcastsRepository
     const podcast = await PodcastModel.findOne({ name });
 
     return podcast ? map<Podcast>(podcast) : undefined;
+  }
+
+  async find({
+    page,
+    limit,
+  }: FindPodcastsRepositoryParams): Promise<Podcast[]> {
+    const skip = (page - 1) * limit;
+
+    const podcasts = await PodcastModel.find().skip(skip).limit(limit);
+
+    return mapCollection<Podcast>(podcasts);
   }
 }
