c739e14f20242d58cdecea671b8d6ef26229b3c4
Marcos Corsi <marcosvcorsi@gmail.com>
2021-04-28 21:43:00 -0300

    test: add list podcasts controller with usecase

---
+++ test/presentation/controllers/ListPodcastsController.spec.ts
@@ -0,0 +1,53 @@
+import { Podcast } from '@/domain/entities/Podcast';
+import { IListPodcastsUseCase } from '@/domain/useCases/listPodcasts/IListPodcastsUseCase';
+import { ListPodcastsController } from '@/presentation/controllers/ListPodcastsController';
+import { Request } from '@/presentation/protocols/http';
+
+const mockRequest = (): Request => ({
+  query: {
+    page: '1',
+    limit: '10',
+  },
+});
+
+const mockListPodcastsUseCase = (): IListPodcastsUseCase => {
+  class ListPodcastsUseCaseStub implements IListPodcastsUseCase {
+    async list(): Promise<Podcast[]> {
+      return [
+        {
+          id: 'anyid',
+          description: 'anydesc',
+          links: ['http://anylink.com'],
+          name: 'anyname',
+        },
+      ];
+    }
+  }
+
+  return new ListPodcastsUseCaseStub();
+};
+
+describe('ListPodcastsController Tests', () => {
+  let listPodcastsController: ListPodcastsController;
+  let listPodcastsUseCaseStub: IListPodcastsUseCase;
+
+  beforeEach(() => {
+    listPodcastsUseCaseStub = mockListPodcastsUseCase();
+    listPodcastsController = new ListPodcastsController(
+      listPodcastsUseCaseStub
+    );
+  });
+
+  it('should call DbListPodcastsUseCase with correct values', async () => {
+    const listSpy = jest.spyOn(listPodcastsUseCaseStub, 'list');
+
+    const request = mockRequest();
+
+    await listPodcastsController.handle(request);
+
+    expect(listSpy).toHaveBeenCalledWith({
+      page: Number(request.query.page),
+      limit: Number(request.query.limit),
+    });
+  });
+});
