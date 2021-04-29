8d535e54cc792700759f84904f17d35ece54706b
Marcos Corsi <marcosvcorsi@gmail.com>
2021-04-28 21:48:12 -0300

    feat: add list podcastas controller internal server error return

--- src/presentation/controllers/ListPodcastsController.ts
+++ src/presentation/controllers/ListPodcastsController.ts
@@ -1,19 +1,23 @@
 import { IListPodcastsUseCase } from '@/domain/useCases/listPodcasts/IListPodcastsUseCase';
 
 import { IController } from '../protocols/controller';
-import { Request, Response } from '../protocols/http';
+import { internalServerError, Request, Response } from '../protocols/http';
 
 export class ListPodcastsController implements IController {
   constructor(private readonly listPodcastsUseCase: IListPodcastsUseCase) {}
 
   async handle(request: Request): Promise<Response> {
-    const { page = 1, limit = 10 } = request.query;
+    try {
+      const { page = 1, limit = 10 } = request.query;
 
-    await this.listPodcastsUseCase.list({
-      page: Number(page),
-      limit: Number(limit),
-    });
+      await this.listPodcastsUseCase.list({
+        page: Number(page),
+        limit: Number(limit),
+      });
 
-    return {} as Response;
+      return {} as Response;
+    } catch (error) {
+      return internalServerError();
+    }
   }
 }
