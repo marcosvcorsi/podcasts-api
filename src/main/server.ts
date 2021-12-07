16ff3287cf8c9009ff7e6dced2727b85b3acdfdc
Marcos Corsi <marcosvcorsi@gmail.com>
2021-12-07 00:20:30 -0300

    chore: teste ci

--- src/main/server.ts
+++ src/main/server.ts
@@ -6,6 +6,6 @@ const logger = pino();
 
 const port = process.env.PORT || 3000;
 
-logger.info('updated new cli');
+logger.info('updated new cli is working');
 
 app.listen(port, () => logger.info(`Server is running on port ${port}`));
